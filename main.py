8b3d995b6a706d40cdec6d1a73a288904eb7a1e3
Joao-H-Luz <joaohluz13@gmail.com>
2025-04-01 16:10:18 -0300

    Classe Cliente

--- main.py
+++ main.py
@@ -12,21 +12,48 @@ class Produto:
     def aplicar_desconto(self, percentual):
         percentual = 1 - (percentual/100)
         self.preco = self.preco * percentual
-    
+
     def verificar_estoque_baixo(self):
         if self.estoque < 5:
             return True
         return False
-    
+
     def __eq__(self, Q2):
         return self.nome == Q2.nome and self.preco == Q2.preco
-    
+
     def __str__(self):
         return (f"Produto: {self.nome} | Preço: R$ {self.preco} | Estoque: {self.estoque}")
-        
 
+
+class Cliente:
+    def __init__(self, nome=str, email=str, pontos_fidelidade=int):
+        self.nome = nome
+        self.email = email
+        self.pontos_fidelidade = 0
+
+    def adicionar_pontos(self, pontos):
+        if pontos > 0:
+            self.pontos_fidelidade = pontos
+
+    def resgatar_pontos(self, pontos):
+        if pontos > 0:
+            self.pontos_fidelidade -= pontos
+
+    def verificar_vip(self):
+        if self.pontos_fidelidade >= 1000:
+            return True
+        return False
+
+    def __eq__(self, Q2):
+        return self.email == Q2.email
+
+    def __iadd__(self, pontos=int):
+        if pontos > 0:
+            self.pontos_fidelidade += pontos
+
+    def __str__(self):
+        return (f"Cliente: {self.nome} | Pontos: {self.pontos_fidelidade}")
 
 
 if __name__ == "__main__":
-        pass
- 
+    pass
